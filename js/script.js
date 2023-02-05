dc557a4e7d1b5486c230713c6a0ace120d7dad71
Enmanuel <enmanuel.sanchez.a@gmail.com>
2023-02-05 16:00:30 -0400

    Add custom grid

--- js/script.js
+++ js/script.js
@@ -4,27 +4,38 @@ const setGrid = document.querySelector('#set-grid');
 let gridSize = 16;
 
 setGrid.addEventListener('click', e => {
-    gridSize = +prompt('What numbers of square do you want per grid?');
+    do {
+        gridSize = +prompt('What numbers of square do you want per grid?');
+    } while (gridSize > 100);
+    
     generateGrid();
 });
 
 function generateGrid() {
-    for (let i = 0; i < gridSize * gridSize ; i++) {
+
+    const oldPixels = document.querySelectorAll('.sketchpad > div');
+
+    oldPixels.forEach(oldPixel => {
+        oldPixel.remove();
+    });
+    
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
         div.style.flexBasis = `${550 / gridSize}px`;
         sketchpad.appendChild(div);
     }
+
+    const pixels = document.querySelectorAll('.pixel');
+    
+    pixels.forEach(pixel => {
+        
+        pixel.addEventListener('mouseover', e => {
+            pixel.classList.add('pixel-hover');
+            pixel.style.backgroundColor = document.querySelector('#select-color').value;
+        });
+        
+    });
 }
 
 generateGrid();
-const pixels = document.querySelectorAll('.pixel');
-
-pixels.forEach(pixel => {
-    
-    pixel.addEventListener('mouseover', e => {
-        pixel.classList.add('pixel-hover');
-        pixel.style.backgroundColor = document.querySelector('#select-color').value;
-    });
-    
-});
