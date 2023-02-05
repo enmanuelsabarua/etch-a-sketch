30ae36b39d106c7789cd5ab65664cf2f8c30f503
Enmanuel <enmanuel.sanchez.a@gmail.com>
2023-02-05 15:52:56 -0400

    Add function to draw in a 16x16 sketchpad

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 const sketchpad = document.querySelector('.sketchpad');
+const setGrid = document.querySelector('#set-grid');
 
-for (let i = 0; i < 16 * 16 ; i++) {
-    const div = document.createElement('div');
-    div.classList.add('pixel');
-    sketchpad.appendChild(div);
+let gridSize = 16;
+
+setGrid.addEventListener('click', e => {
+    gridSize = +prompt('What numbers of square do you want per grid?');
+    generateGrid();
+});
+
+function generateGrid() {
+    for (let i = 0; i < gridSize * gridSize ; i++) {
+        const div = document.createElement('div');
+        div.classList.add('pixel');
+        div.style.flexBasis = `${550 / gridSize}px`;
+        sketchpad.appendChild(div);
+    }
 }
+
+generateGrid();
+const pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach(pixel => {
+    
+    pixel.addEventListener('mouseover', e => {
+        pixel.classList.add('pixel-hover');
+        pixel.style.backgroundColor = document.querySelector('#select-color').value;
+    });
+    
+});
