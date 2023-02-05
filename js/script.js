a4fd70da50e60b6ec5eecb7ca09aaeb32e041774
Enmanuel <enmanuel.sanchez.a@gmail.com>
2023-02-05 16:56:30 -0400

    Add function to draw a rainbow

--- js/script.js
+++ js/script.js
@@ -50,6 +50,21 @@ function generateGrid() {
         }
 
     });
+
+    let rainbowActivate = 1;
+    const rainbow = document.querySelector('#rainbow');
+
+    rainbow.addEventListener('click', e => {
+        if (rainbowActivate) {
+            rainbowActivate = 0;
+            e.target.textContent = 'Stop';
+            drawRainbow(pixels);
+        } else {
+            rainbowActivate = 1;
+            e.target.textContent = 'Rainbow';
+            draw(pixels);
+        }
+    });
 }
 
 function draw(pixels) {
@@ -69,6 +84,21 @@ function erase(pixels) {
         pixel.addEventListener('mouseover', e => {
             pixel.classList.add('pixel-hover');
             pixel.style.backgroundColor = 'white';
+        });
+        
+    });
+}
+
+function drawRainbow(pixels) {
+    pixels.forEach(pixel => {
+        
+        pixel.addEventListener('mouseover', e => {
+            let randomColor1 = Math.floor(Math.random() * 255);
+            let randomColor2 = Math.floor(Math.random() * 255);
+            let randomColor3 = Math.floor(Math.random() * 255);
+
+            pixel.classList.add('pixel-hover');
+            pixel.style.backgroundColor = `rgb(${randomColor1}, ${randomColor2}, ${randomColor3})`;
         });
         
     });
