2612db8b97b8f1386be3f3f6daab5981d836d29c
Enmanuel <enmanuel.sanchez.a@gmail.com>
2023-02-05 16:32:09 -0400

    Add function to draw and erase pixels

--- js/script.js
+++ js/script.js
@@ -3,22 +3,26 @@ const setGrid = document.querySelector('#set-grid');
 
 let gridSize = 16;
 
+// Ask for the grid size
 setGrid.addEventListener('click', e => {
     do {
         gridSize = +prompt('What numbers of square do you want per grid?');
     } while (gridSize > 100);
-    
+
     generateGrid();
 });
 
+// Create the grid
 function generateGrid() {
 
     const oldPixels = document.querySelectorAll('.sketchpad > div');
 
+    // Erase the old grid when selected a new one
     oldPixels.forEach(oldPixel => {
         oldPixel.remove();
     });
     
+    // Create a grid with dynamic size
     for (let i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
@@ -27,12 +31,44 @@ function generateGrid() {
     }
 
     const pixels = document.querySelectorAll('.pixel');
-    
+    draw(pixels);
+
+    // Erase or draw pixels
+    const eraser = document.querySelector('#eraser');
+    let drawOrErase = 1;
+
+    eraser.addEventListener('click', e => {
+        
+        if (drawOrErase) {
+            drawOrErase = 0;
+            e.target.textContent = 'Draw';
+            erase(pixels);
+        } else {
+            drawOrErase = 1;
+            e.target.textContent = 'Eraser';
+            draw(pixels);
+        }
+
+    });
+}
+
+function draw(pixels) {
     pixels.forEach(pixel => {
         
         pixel.addEventListener('mouseover', e => {
             pixel.classList.add('pixel-hover');
             pixel.style.backgroundColor = document.querySelector('#select-color').value;
+        });
+        
+    });
+}
+
+function erase(pixels) {
+    pixels.forEach(pixel => {
+            
+        pixel.addEventListener('mouseover', e => {
+            pixel.classList.add('pixel-hover');
+            pixel.style.backgroundColor = 'white';
         });
         
     });
